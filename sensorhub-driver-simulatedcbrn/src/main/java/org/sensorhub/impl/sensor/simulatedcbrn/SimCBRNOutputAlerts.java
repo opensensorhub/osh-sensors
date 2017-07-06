c2f63fbe4ab03faa6676f2f20afa708e08b35718
Ian Patterson <tipatterson31@gmail.com>
2017-07-05 20:42:38 -0500

    Updated outputs for Level String values

--- sensorhub-driver-simulatedcbrn/src/main/java/org/sensorhub/impl/sensor/simulatedcbrn/SimCBRNOutputAlerts.java
+++ sensorhub-driver-simulatedcbrn/src/main/java/org/sensorhub/impl/sensor/simulatedcbrn/SimCBRNOutputAlerts.java
@@ -460,11 +460,15 @@ public class SimCBRNOutputAlerts extends AbstractSensorOutput<SimCBRNSensor>
         {
             return "NONE";
         }
-        else if(threatLevel > min_threat && threatLevel <= max_threat/2)
+        else if(threatLevel > min_threat && threatLevel <= max_threat / 3)
+        {
+            return "LOW";
+        }
+        else if(threatLevel > max_threat / 3 && threatLevel <= max_threat * 2 / 3)
         {
             return "MEDIUM";
         }
-        else if(threatLevel > max_threat/2)
+        else if(threatLevel > max_threat * 2 / 3)
         {
             return "HIGH";
         }
